317900dfc8f5a5483f74b37a61b20ba31d4f4870
Önder Sümer <ondersumer07@gmail.com>
2020-03-17 11:35:01 +0300

    sw changes

--- sw.js
+++ sw.js
@@ -1,49 +1,3 @@
-// const cacheName = 'offline-vinematik';
-// const staticAssets = [
-//   './',
-//   './offline.html',
-//   './css/offline.css',
-// ];
-//
-// self.addEventListener('install', async e => {
-//   const cache = await caches.open(cacheName);
-//   await cache.addAll(staticAssets);
-//   return self.skipWaiting();
-// });
-//
-// self.addEventListener('activate', e => {
-//   self.clients.claim();
-// });
-//
-// self.addEventListener('fetch', async e => {
-//   const req = e.request;
-//   const url = new URL(req.url);
-//
-//   if (url.origin === location.origin) {
-//     e.respondWith(cacheFirst(req));
-//   } else {
-//     e.respondWith(networkAndCache(req));
-//   }
-// });
-//
-// async function cacheFirst(req) {
-//   const cache = await caches.open(cacheName);
-//   const cached = await cache.match(req);
-//   return cached || fetch(req);
-// }
-//
-// async function networkAndCache(req) {
-//   const cache = await caches.open(cacheName);
-//   try {
-//     const fresh = await fetch(req);
-//     await cache.put(req, fresh.clone());
-//     return fresh;
-//   } catch (e) {
-//     const cached = await cache.match(req);
-//     return cached;
-//   }
-// }
-
 //CACHING FILES//
 const filesToCache = [
   './',
@@ -53,44 +7,84 @@ const filesToCache = [
 
 const staticCacheName = 'pages-cache-v2';
 
-self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
+// self.addEventListener('install', event => {
+//   console.log('Attempting to install service worker and cache static assets');
+//   event.waitUntil(
+//     caches.open(staticCacheName)
+//     .then(cache => {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
+//
+// //LOADING FILES WHEN OFFLINE//
+// self.addEventListener('fetch', event => {
+//   console.log('Fetch event for ', event.request.url);
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(response => {
+//       if (response) {
+//         console.log('Found ', event.request.url, ' in cache');
+//         return response;
+//       }
+//       console.log('Network request for ', event.request.url);
+//       return fetch(event.request)
+//
+//         .then(response => {
+//           // TODO 5 - Respond with custom 404 page
+//           return caches.open(staticCacheName).then(cache => {
+//             cache.put(event.request.url, response.clone());
+//             return response;
+//           });
+//         });
+//
+//
+//     }).catch(error => {
+//       console.log('Error, ', error);
+//       return caches.match('./offline.html');
+//     })
+//   );
+// });
+
+
+self.addEventListener('install', function(event) {
+  // Put `offline.html` page into cache
+  var offlineRequest = new Request('offline.html');
   event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
+    fetch(offlineRequest).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        console.log('[oninstall] Cached offline page', response.url);
+        return cache.put(offlineRequest, response);
+      });
     })
   );
 });
 
-//LOADING FILES WHEN OFFLINE//
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-
-        .then(response => {
-          // TODO 5 - Respond with custom 404 page
-          return caches.open(staticCacheName).then(cache => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          });
+self.addEventListener('fetch', function(event) {
+  // Only fall back for HTML documents.
+  var request = event.request;
+  // && request.headers.get('accept').includes('text/html')
+  if (request.method === 'GET') {
+    // `fetch()` will use the cache when possible, to this examples
+    // depends on cache-busting URL parameter to avoid the cache.
+    event.respondWith(
+      fetch(request).catch(function(error) {
+        // `fetch()` throws an exception when the server is unreachable but not
+        // for valid HTTP responses, even `4xx` or `5xx` range.
+        console.error(
+          '[onfetch] Failed. Serving cached offline fallback ' +
+          error
+        );
+        return caches.open('offline').then(function(cache) {
+          return cache.match('offline.html');
         });
-
-
-    }).catch(error => {
-      console.log('Error, ', error);
-      return caches.match('./offline.html');
-    })
-  );
+      })
+    );
+  }
+  // Any other handlers come here. Without calls to `event.respondWith()` the
+  // request will be handled without the ServiceWorker.
 });
+
 // TODO 6 - Respond with custom offline page
 
 self.addEventListener('activate', event => {
