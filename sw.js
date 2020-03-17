0bd15529c4e17cfde5ffe47b01f1245750d194ce
Önder Sümer <ondersumer07@gmail.com>
2020-03-17 10:50:01 +0300

    sw changes

--- sw.js
+++ sw.js
@@ -44,6 +44,7 @@
 //   }
 // }
 
+//CACHING FILES//
 const filesToCache = [
   '/',
   'index.html',
@@ -58,6 +59,29 @@ self.addEventListener('install', event => {
     caches.open(staticCacheName)
     .then(cache => {
       return cache.addAll(filesToCache);
+    })
+  );
+});
+
+//LOADING FILES WHEN OFFLINE//
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
     })
   );
 });
