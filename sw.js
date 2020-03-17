2229284b3297d976baae6d898d01f328788fa563
Önder Sümer <ondersumer07@gmail.com>
2020-03-17 11:36:54 +0300

    fuck go back sw

--- sw.js
+++ sw.js
@@ -7,84 +7,44 @@ const filesToCache = [
 
 const staticCacheName = 'pages-cache-v2';
 
-// self.addEventListener('install', event => {
-//   console.log('Attempting to install service worker and cache static assets');
-//   event.waitUntil(
-//     caches.open(staticCacheName)
-//     .then(cache => {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
-//
-// //LOADING FILES WHEN OFFLINE//
-// self.addEventListener('fetch', event => {
-//   console.log('Fetch event for ', event.request.url);
-//   event.respondWith(
-//     caches.match(event.request)
-//     .then(response => {
-//       if (response) {
-//         console.log('Found ', event.request.url, ' in cache');
-//         return response;
-//       }
-//       console.log('Network request for ', event.request.url);
-//       return fetch(event.request)
-//
-//         .then(response => {
-//           // TODO 5 - Respond with custom 404 page
-//           return caches.open(staticCacheName).then(cache => {
-//             cache.put(event.request.url, response.clone());
-//             return response;
-//           });
-//         });
-//
-//
-//     }).catch(error => {
-//       console.log('Error, ', error);
-//       return caches.match('./offline.html');
-//     })
-//   );
-// });
-
-
-self.addEventListener('install', function(event) {
-  // Put `offline.html` page into cache
-  var offlineRequest = new Request('offline.html');
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    fetch(offlineRequest).then(function(response) {
-      return caches.open('offline').then(function(cache) {
-        console.log('[oninstall] Cached offline page', response.url);
-        return cache.put(offlineRequest, response);
-      });
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  // Only fall back for HTML documents.
-  var request = event.request;
-  // && request.headers.get('accept').includes('text/html')
-  if (request.method === 'GET') {
-    // `fetch()` will use the cache when possible, to this examples
-    // depends on cache-busting URL parameter to avoid the cache.
-    event.respondWith(
-      fetch(request).catch(function(error) {
-        // `fetch()` throws an exception when the server is unreachable but not
-        // for valid HTTP responses, even `4xx` or `5xx` range.
-        console.error(
-          '[onfetch] Failed. Serving cached offline fallback ' +
-          error
-        );
-        return caches.open('offline').then(function(cache) {
-          return cache.match('offline.html');
-        });
-      })
-    );
-  }
-  // Any other handlers come here. Without calls to `event.respondWith()` the
-  // request will be handled without the ServiceWorker.
-});
+//LOADING FILES WHEN OFFLINE//
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
 
+        .then(response => {
+          // TODO 5 - Respond with custom 404 page
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+
+
+    }).catch(error => {
+      console.log('Error, ', error);
+      return caches.match('./offline.html');
+    })
+  );
+});
 // TODO 6 - Respond with custom offline page
 
 self.addEventListener('activate', event => {
