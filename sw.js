591672e8ca3a44d5c0ffd2ef4d216a8cbc4fd846
Önder Sümer <ondersumer07@gmail.com>
2020-03-17 10:46:46 +0300

    sw changes

--- sw.js
+++ sw.js
@@ -1,45 +1,63 @@
-const cacheName = 'offline-vinematik';
-const staticAssets = [
-  // './',
-  './offline.html',
-  './css/offline.css',
+// const cacheName = 'offline-vinematik';
+// const staticAssets = [
+//   './',
+//   './offline.html',
+//   './css/offline.css',
+// ];
+//
+// self.addEventListener('install', async e => {
+//   const cache = await caches.open(cacheName);
+//   await cache.addAll(staticAssets);
+//   return self.skipWaiting();
+// });
+//
+// self.addEventListener('activate', e => {
+//   self.clients.claim();
+// });
+//
+// self.addEventListener('fetch', async e => {
+//   const req = e.request;
+//   const url = new URL(req.url);
+//
+//   if (url.origin === location.origin) {
+//     e.respondWith(cacheFirst(req));
+//   } else {
+//     e.respondWith(networkAndCache(req));
+//   }
+// });
+//
+// async function cacheFirst(req) {
+//   const cache = await caches.open(cacheName);
+//   const cached = await cache.match(req);
+//   return cached || fetch(req);
+// }
+//
+// async function networkAndCache(req) {
+//   const cache = await caches.open(cacheName);
+//   try {
+//     const fresh = await fetch(req);
+//     await cache.put(req, fresh.clone());
+//     return fresh;
+//   } catch (e) {
+//     const cached = await cache.match(req);
+//     return cached;
+//   }
+// }
+
+const filesToCache = [
+  '/',
+  'index.html',
+  'css/offlime.css',
 ];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
